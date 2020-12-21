67296dd270d505c3532d242c35baec4fc8a32f02
Abhinandan Kushwaha <abhinandan@amci.net.in>
2020-12-21 21:38:05 +0530

    Added the Autofill feature

--- src/components/simpleContainer.js
+++ src/components/simpleContainer.js
@@ -1,215 +1,340 @@
-import { React, useState } from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import { React, useState } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
-import ButtonAppBar from './AppBar'
-import Avatar from "@material-ui/core/Avatar";
-import FormHelperText from "@material-ui/core/FormHelperText";
-// import FormControl from "@material-ui/core/FormControl";
-// import InputLabel from "@material-ui/core/InputLabel";
-// import Select from "@material-ui/core/Select";
-// import MenuItem from "@material-ui/core/MenuItem";
+import ButtonAppBar from "./AppBar";
+import QRCode from "qrcode.react";
+import moment from "moment";
 
-
-import { makeStyles } from '@material-ui/core/styles';
-
+const axios = require("axios");
 const Joi = require("joi");
-
 const Schema = Joi.object({
-  subjectName: Joi.string().required(),
-  description: Joi.string().required(),
-  displayPriority: Joi.number().allow(''),
-  imgBase: Joi.string()
-
-
+  userName:Joi.string()
+  .pattern(/^[A-Za-z]+$/)
+  .required(),
+  firstName: Joi.string()
+    .pattern(/^[A-Za-z]+$/)
+    .required(),
+  middleName: Joi.string()
+    .pattern(/^[A-Za-z]+$/)
+    .allow(null),
+  lastName: Joi.string()
+    .pattern(/^[A-Za-z]+$/)
+    .allow(null),
+  birthday: Joi.date().allow(null),
+  gender: Joi.string().allow(null),
+  mobile: Joi.string()
+    .length(10)
+    .pattern(/^[0-9]+$/)
+    .required(),
+  primaryEmail: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
+  country: Joi.string().allow(null),
+  stateProvince: Joi.string().allow(null),
+  city: Joi.string().allow(null),
+  postalCode: Joi.string()
+    .length(6)
+    .pattern(/^[0-9]+$/)
+    .allow(""),
 });
-
-
-const useStyles = makeStyles((theme) => ({
-
-  rootPicture: {
-    display: "flex",
-    "& > *": {
-      margin: theme.spacing(1),
-      width: theme.spacing(8),
-      height: theme.spacing(8),
-    },
-  },
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    }, roots: {
-        '& > *': {
-          margin: theme.spacing(1),
-          width: '25ch',
-        },
-      },
-  }));
-
-
-var sb1, description, priority, check = true,imagesHere;
-// var priorityCodes = ['1','2','3','4','5']
-
-
+var postURL =
+  "https://bgtnp5wnta.execute-api.ap-south-1.amazonaws.com/dev/qrCode";
+// var afterPost = false;
+var userName,
+firstName,
+  middleName,
+  lastName,
+  birthday,
+  gender,
+  mobile,
+  check = true,
+  primaryEmail,
+  country,
+  stateProvince,
+  city,
+  postalCode;
 export default function SimpleContainer() {
+  var [data, setData] = useState({});
+  var [afterPost, setAfterPost] = useState(false);
+  console.log("afterpost is", afterPost);
+  const [qrValue, setQrValue] = useState("Manish Madhavan");
+  const Qrcode = () => (
+    <Grid style={{ paddingTop: "5%" }}>
+      <QRCode
+        id="qr-gen"
+        value={qrValue}
+        size={200}
+        level={"L"}
+        includeMargin={true}
+      />
+    </Grid>
+  );
+  function handleDataChange(event) {
+    console.log("event.target value is ", event.target.id, event.target.value);
+    const { error, value } = Schema.validate({
+      ...data,
+      [event.target.id]: event.target.value,
+    });
+    console.log("value is", value);
+    if (error) {
+      console.log("error is", error);
+      check = true;
+    } else {
+      console.log("value is", value);
+      console.log("type ofvalue is", typeof value);
+      check = false;
+      // setQrValue(myJSON);
+    }
+    setData(value);
+    console.log("data state is", data);
+    if (event.target.value == null) {
+      console.log("yes");
+    }
+  }
 
-  var [data, setData] = useState({})
-  var [image,setImage]=useState( require('./../assets/blank-avatar-photo.jpg'));
+  function getRandomName(){
+    let name=''
+    let chars=['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M','q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'];
+    for(let i=0;i<8;i++){
+      name+=chars[Math.floor(Math.random()*52)]
+    }
+    return name;
+  }
 
+  function getRandomMobile(){
+    let mobile='';
+    for(let i=0;i<10;i++){
+      mobile+=Math.floor(Math.random()*10);
+    }
+    return  mobile;
+  }
+  function getRandomZip(){
+    let zip='';
+    for(let i=0;i<6;i++){
+      zip+=Math.floor(Math.random()*10);
+    }
+    return  zip;
+  }
 
-  var imgFile;
-  const picFunction = async (e) => {
-    imgFile = e.target.files[0];
-    var tempImgURL = URL.createObjectURL(imgFile);
-    setImage(tempImgURL);
-    var b64 = await convertBase64(imgFile);
-    console.log(b64);
+  function onAutoFill() {
+    userName=getRandomName();
+    firstName=getRandomName();
+    middleName=getRandomName();
+    lastName=getRandomName();
+    birthday='1/1/2020';
+    gender=Math.random()>.5?'male':'female';
+    mobile=getRandomMobile();
+    primaryEmail=userName+'@gmail.com';
+    country='India';
+    stateProvince='Karnataka';
+    city='Bengaluru';
+    postalCode=getRandomZip();
+    check=false;
+    setData({...data,userName,firstName,middleName,lastName,birthday,gender,mobile,primaryEmail,country,stateProvince,city,postalCode});
+  }
 
-    var eve = {
-      target: {
-        id: "imgBase",
-        value: b64 || " ",
+  async function onSubmitClick() {
+    console.log("from the on submit function", afterPost);
+    var payload = await {
+      phone_number:data.mobile?'+91'+data.mobile:"",
+      "custom:college_id": "507f1f77bcf86cd799439011",
+      "custom:user_type": "user",
+      email: data.primaryEmail,
+      generated_by: "50721f77bcf86cd799439011",
+      generated_by_name: "shyamsagar",
+      user_data: {
+        username:data.userName,
+        firstName: data.firstName,
+        middleName: data.middleName || "",
+        lastName: data.lastName || "",
+        fullName:
+          data.firstName +
+            " " +
+            (data.middleName || "") +
+            " " +
+            data.lastName || "",
+        birthdate: moment(data.birthdate).format("L"),
+        gender: data.gender || "",
+        mobile: "+91" + data.mobile,
+        userThumbnailPath: "https://www.amci.net.in/logo",
+        primaryEmail: data.primaryEmail,
+        secondaryEmails: [],
+        country: data.country || "",
+        stateProvince: data.stateProvince || "",
+        city: data.city || "",
       },
     };
-    handleDataChange(eve);
-  };
-
-
-  function convertBase64(file) {
-    return new Promise((res, rej) => {
-      const filereader = new FileReader();
-      filereader.readAsDataURL(file);
-      filereader.onload = () => {
-        res(filereader.result);
-      };
-
-      filereader.onerror = (errors) => {
-        rej(errors);
-      };
-    });
+    axios
+      .post(postURL, payload)
+      .then((res) => {
+        console.log("response is ", res);
+        var myJSON = JSON.stringify(res.data);
+        setQrValue(myJSON);
+        setAfterPost(true);
+      })
+      .catch((err) => {
+        console.log("error is", err);
+        console.log("error res is", err.response);
+      });
+    console.log("payload is ", payload);
   }
-
-
-
-
-
-
-  const classes = useStyles();
-
-  function handleDataChange(event) {
-    console.log("event.target value is ", event.target.id, event.target.value)
-    const { error, value } = Schema.validate({ ...data, [event.target.id]: event.target.value, });
-    console.log("value is", value)
-    if (error) {
-      console.log("error is", error)
-      check = true
-    } else {
-      console.log("value is", value)
-      check = false;
-    }
-    setData(value)
-    console.log("data state is", data)
-    if (event.target.value == null) {
-      console.log("yes")
-    }
-  }
-
-
   return (
     <div>
       <ButtonAppBar />
       <CssBaseline />
       <Container maxWidth="sm">
-        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '90vh' }} >
-
-
-
-        <Grid item xs={12} style={{ textAlign: "center" }}>
-            <Button size="large">
-              <label htmlFor="file" id="uploadBtn">
-                <div className={classes.rootPicture}>
-                  <Avatar alt="Profile Picture" src={image} />
-                </div>
-                <FormHelperText>Subject Img</FormHelperText>
-
-              </label>
-            </Button>
-            <input
-              type="file"
-              id="file"
-              value={imagesHere||""}
-              onChange={(e) => {
-                picFunction(e);
-              }}
-              style={{ display: "none" }}
-            />
-          </Grid>
-
-
-
-
+        <Typography
+          component="div"
+          style={{ backgroundColor: "#CFE8FC", height: "90vh" }}
+        >
           <Grid item xs={12} style={{ textAlign: "center" }}>
+            User Name:
             <TextField
-              id="subjectName"
-              label="Subject Name"
-              value={sb1}
+            style={{marginLeft:20}}
+              required
+              id="userName"
+              // label="User Name"
+              value={userName}
               onChange={handleDataChange}
             />
           </Grid>
-
           <Grid item xs={12} style={{ textAlign: "center" }}>
+          First Name:
             <TextField
-              id="description"
-              label="Description"
-              value={description}
+            style={{marginLeft:20}}
+              required
+              id="firstName"
+              // label="First Name"
+              value={firstName}
               onChange={handleDataChange}
             />
           </Grid>
-
           <Grid item xs={12} style={{ textAlign: "center" }}>
+          Middle Name:
             <TextField
-              id="displayPriority"
-              label="Priority"
-              value={priority}
+            style={{marginLeft:20}}
+              id="middleName"
+              // label="Middle Name"
+              value={middleName}
               onChange={handleDataChange}
             />
           </Grid>
-
-          {/* <Grid item xs={12} style={{ textAlign: "center" }}>
-          <FormControl> 
-          <InputLabel id="countryCode">Priority</InputLabel>
-
-            <Select
-              id="displayPriority"
-              label="Priority"
-              value={priority}
+          <Grid item xs={12} style={{ textAlign: "center" }}>
+          Last Name:
+            <TextField
+            style={{marginLeft:20}}
+              id="lastName"
+              // label="Last Name"
+              value={lastName}
               onChange={handleDataChange}
-              
-            >
-              {countryCodes.map((ele) => (
-                <MenuItem value={ele} key={ele}>
-                  {ele}
-                </MenuItem>
-              ))}
-
-            </Select>
-            <FormHelperText>Select your Subject Priority here</FormHelperText>
-
-            </FormControl>
-          </Grid> */}
-
-          
+            />
+          </Grid>
+          <Grid item xs={12} style={{ textAlign: "center" }}>
+          Birthday:
+            <TextField
+            style={{marginLeft:20}}
+              id="birthday"
+              // label="Birthday"
+              value={birthday}
+              onChange={handleDataChange}
+            />
+          </Grid>
+          <Grid item xs={12} style={{ textAlign: "center" }}>
+          Gender:
+            <TextField
+            style={{marginLeft:20}}
+              id="gender"
+              // label="Gender"
+              value={gender}
+              onChange={handleDataChange}
+            />
+          </Grid>
+          <Grid item xs={12} style={{ textAlign: "center" }}>
+          Mobile:
+            <TextField
+            style={{marginLeft:20}}
+              required
+              id="mobile"
+              // label="Mobile"
+              value={mobile}
+              onChange={handleDataChange}
+            />
+          </Grid>
+          <Grid item xs={12} style={{ textAlign: "center" }}>
+          Primary Email:
+            <TextField
+            style={{marginLeft:20}}
+              required
+              id="primaryEmail"
+              // label="Primary Email"
+              value={primaryEmail}
+              onChange={handleDataChange}
+            />
+          </Grid>
+          <Grid item xs={12} style={{ textAlign: "center" }}>
+            Country:
+            <TextField
+            style={{marginLeft:20}}
+              id="country"
+              // label="country"
+              value={country}
+              onChange={handleDataChange}
+            />
+          </Grid>
+          <Grid item xs={12} style={{ textAlign: "center" }}>
+          State:
+            <TextField
+            style={{marginLeft:20}}
+              id="stateProvince"
+              // label="State"
+              value={stateProvince}
+              onChange={handleDataChange}
+            />
+          </Grid>
+          <Grid item xs={12} style={{ textAlign: "center" }}>
+          City:
+            <TextField
+            style={{marginLeft:20}}
+              id="city"
+              // label="City"
+              value={city}
+              onChange={handleDataChange}
+            />
+          </Grid>
+          <Grid item xs={12} style={{ textAlign: "center" }}>
+          Zip:
+            <TextField
+            style={{marginLeft:20}}
+              id="postalCode"
+              // label="Zip"
+              value={postalCode}
+              onChange={handleDataChange}
+            />
+          </Grid>
           <br />
-
-          <Button variant='contained' color='primary' disabled={check}>Submit</Button>
+          <Button
+          style={{marginRight:30}}
+            variant="contained"
+            color="primary"
+            onClick={onAutoFill}
+          >
+            Auto fill
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            disabled={check}
+            onClick={onSubmitClick}
+          >
+            Submit
+          </Button>
+          {afterPost ? <Qrcode /> : ""}
+          <br />
+          {/* </Grid> */}
         </Typography>
       </Container>
     </div>
